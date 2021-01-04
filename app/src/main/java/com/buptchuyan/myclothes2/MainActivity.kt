a089bdeb738fa24a163b2167d16981677c4c4dac
史XX <1653891226@qq.com>
2021-01-04 22:30:40 +0800

    Initial commit

---
+++ app/src/main/java/com/buptchuyan/myclothes2/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.buptchuyan.myclothes2
+
+//import android.R
+import android.os.Bundle
+import android.view.View
+import android.widget.LinearLayout
+import androidx.appcompat.app.AppCompatActivity
+
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    //    val background: LinearLayout
+     //   background = findViewById<View>(R.id.background) as LinearLayout
+        setContentView(R.layout.activity_main)
+    }
+}
